dafc94568ebdcc4678412be6b93f9c073cc3d2f9
Felix Schwarz <me@schwarzf.de>
2024-01-16 11:09:23 +0100

    Red errors

--- BoCompiler/src/de/schwarzf/boco/Main.java
+++ BoCompiler/src/de/schwarzf/boco/Main.java
@@ -7,6 +7,26 @@ import de.schwarzf.boco.minsk.SyntaxToken;
 import java.io.Console;
 
 public class Main {
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
     public static void main(String[] args) {
         System.out.println("BoCo: SchwarzF's BoCompiler for the BoCode Programming Language");
         while (true) {
@@ -24,7 +44,7 @@ public class Main {
 
             if (parser.getDiagnostics().size() > 0) {
                 for (String diagnostic : parser.getDiagnostics()) {
-                    System.out.println(diagnostic);
+                    System.out.println(ANSI_RED + diagnostic + ANSI_RESET);
                 }
             }
 
