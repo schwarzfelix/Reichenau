785d8fd993bb8583c4efea1e3f053ccba35ed854
Felix Schwarz <me@schwarzf.de>
2024-03-16 14:05:35 +0100

    fix e5 00:33:00 (hh:mm:ss)

--- BoCompiler/src/de/schwarzf/boco/minsk/codeAnalysis/syntax/Lexer.java
+++ BoCompiler/src/de/schwarzf/boco/minsk/codeAnalysis/syntax/Lexer.java
@@ -52,127 +52,109 @@ final class Lexer {
         return text.charAt(index);
     }
 
-    private void nextChar(){
-        position++;
-    }
 
-    // nextToken()/lex() is the heart of the lexer
     public SyntaxToken lex(){
 
         this.start = this.position;
         this.kind = SyntaxKind.BAD_TOKEN;
         this.value = null;
 
-        if (digits.contains(getCurrent())) {
 
-            readNumberToken();
 
-        } else if (getCurrent() == ' ' || getCurrent() == '\r' || getCurrent() == '\n') {
+        switch (getCurrent()) {
+            case '\0':
+                this.kind = SyntaxKind.END_OF_FILE_TOKEN;
+                break;
+            case '+':
+                this.kind = SyntaxKind.PLUS_TOKEN;
+                position++;
+                break;
+            case '-':
+                this.kind = SyntaxKind.MINUS_TOKEN;
+                position++;
+                break;
+            case '*':
+                this.kind = SyntaxKind.STAR_TOKEN;
+                position++;
+                break;
+            case '/':
+                this.kind = SyntaxKind.SLASH_TOKEN;
+                position++;
+                break;
+            case '(':
+                this.kind = SyntaxKind.OPEN_PARENTHESIS_TOKEN;
+                position++;
+                break;
+            case ')':
+                this.kind = SyntaxKind.CLOSE_PARENTHESIS_TOKEN;
+                position++;
+                break;
 
-            readWhiteSpace();
-
-        } else if (Character.isLetter(getCurrent())) {
-
-            readIdentifierOrKeyword();
-
-        } else {
-
-            switch (getCurrent()) {
-                case '\0':
-                    this.kind = SyntaxKind.END_OF_FILE_TOKEN;
+            case '&':
+                if (lookAhead() == '&') {
+                    this.kind = SyntaxKind.AMPERSAND_AMPERSAND_TOKEN;
+                    position += 2;
+                    break;
+                }
+                break;
+            case '|':
+                if (lookAhead() == '|') {
+                    this.kind = SyntaxKind.PIPE_PIPE_TOKEN;
+                    position += 2;
+                    break;
+                }
+                break;
+            case '=':
+                if (lookAhead() == '=') {
+                    this.kind = SyntaxKind.EQUALS_EQUALS_TOKEN;
+                    position += 2;
+                    break;
+                } else {
+                    this.kind = SyntaxKind.EQUALS_TOKEN;
                     position++;
                     break;
-                case '+':
-                    this.kind = SyntaxKind.PLUS_TOKEN;
+                }
+            case '!':
+                if (lookAhead() == '=') {
+                    this.kind = SyntaxKind.BANG_EQUALS_TOKEN;
+                    position += 2;
+                    break;
+                } else {
+                    this.kind = SyntaxKind.BANG_TOKEN;
                     position++;
                     break;
-                case '-':
-                    this.kind = SyntaxKind.MINUS_TOKEN;
-                    position++;
-                    break;
-                case '*':
-                    this.kind = SyntaxKind.STAR_TOKEN;
-                    position++;
-                    break;
-                case '/':
-                    this.kind = SyntaxKind.SLASH_TOKEN;
-                    position++;
-                    break;
-                case '(':
-                    this.kind = SyntaxKind.OPEN_PARENTHESIS_TOKEN;
-                    position++;
-                    break;
-                case ')':
-                    this.kind = SyntaxKind.CLOSE_PARENTHESIS_TOKEN;
-                    position++;
-                    break;
-
-                case '&':
-                    if (lookAhead() == '&') {
-                        this.kind = SyntaxKind.AMPERSAND_AMPERSAND_TOKEN;
-                        position += 2;
-                        break;
-                    }
-                    break;
-                case '|':
-                    if (lookAhead() == '|') {
-                        this.kind = SyntaxKind.PIPE_PIPE_TOKEN;
-                        position += 2;
-                        break;
-                    }
-                    break;
-                case '=':
-                    if (lookAhead() == '=') {
-                        this.kind = SyntaxKind.EQUALS_EQUALS_TOKEN;
-                        position += 2;
-                        break;
-                    } else {
-                        this.kind = SyntaxKind.EQUALS_TOKEN;
-                        position++;
-                        break;
-                    }
-                case '!':
-                    if (lookAhead() == '=') {
-                        this.kind = SyntaxKind.BANG_EQUALS_TOKEN;
-                        position += 2;
-                        break;
-                    } else {
-                        this.kind = SyntaxKind.BANG_TOKEN;
-                        position++;
-                        break;
-                    }
-                default:
+                }
+            case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
+                readNumberToken();
+                break;
+            case ' ': case '\r': case '\n': case '\t':
+                readWhiteSpace();
+                break;
+            default:
+                // Unicode whitespace check missing (e5 00:21:40)
+                if (Character.isLetter(getCurrent())) {
+                    readIdentifierOrKeyword();
+                } else {
                     this.diagnostics.reportBadCharacter(position, getCurrent());
                     this.position++;
-                    break;
-            }
-
+                }
+                break;
         }
 
         int length = this.position - this.start;
+        int end = this.start + length;
         String text = SyntaxFacts.getText(this.kind);
         if (text == null) {
-            text = this.text.substring(start, length);
+            text = this.text.substring(start, end);
         }
 
-        return new SyntaxToken(this.kind, this.start, text, (int)this.value);
-    }
-
-    private void readIdentifierOrKeyword() {
-        while (Character.isLetter(getCurrent())) {
-            nextChar();
-        }
-
-        int length = position - start;
-        String text = this.text.substring(start, start + length);
-
-        this.kind = SyntaxFacts.getKeywordKind(text);
+        return new SyntaxToken(this.kind, this.start, text, this.value);
     }
 
     private void readWhiteSpace() {
 
         while (getCurrent() == ' ' || getCurrent() == '\r' || getCurrent() == '\n') {
-            nextChar();
+            position++;
         }
 
         this.kind = SyntaxKind.WHITESPACE;
@@ -182,7 +164,7 @@ final class Lexer {
     private void readNumberToken(){
 
         while (digits.contains(getCurrent())) {
-            nextChar();
+            position++;
         }
         int length = position - start;
         String text = this.text.substring(start, start + length);
@@ -197,5 +179,16 @@ final class Lexer {
 
         this.value = value;
         this.kind = SyntaxKind.NUMBER_TOKEN;
+    }
+
+    private void readIdentifierOrKeyword() {
+        while (Character.isLetter(getCurrent())) {
+            position++;
+        }
+
+        int length = position - start;
+        String text = this.text.substring(start, start + length);
+
+        this.kind = SyntaxFacts.getKeywordKind(text);
     }
 }
