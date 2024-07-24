4a4c18b4e4fc6ca211bba03c0a43c63c63786da6
Felix Schwarz <me@schwarzf.de>
2024-07-24 22:37:39 +0200

    stack machine

--- Reichenau/src/main/java/de/schwarzf/reichenau/intermediateCode/StackMachine.java
+++ Reichenau/src/main/java/de/schwarzf/reichenau/intermediateCode/StackMachine.java
@@ -1,6 +1,5 @@
 package de.schwarzf.reichenau.intermediateCode;
 
-import java.util.ArrayList;
 import java.util.Queue;
 import java.util.Stack;
 
@@ -16,8 +15,23 @@ public class StackMachine {
 
     public void execute() {
         while (!input.isEmpty()) {
-
+            IntermediateCodeNode node = input.poll();
+            switch (node.getKind()) {
+                case IntermediateCodeNodeKind.OPERAND -> stack.push(((IntermediateCodeOperand)node).getValue());
+                case IntermediateCodeNodeKind.ADDITION -> stack.push((int)stack.pop() + (int)stack.pop());
+                case IntermediateCodeNodeKind.SUBTRACTION -> stack.push((int)stack.pop() - (int)stack.pop());
+                case IntermediateCodeNodeKind.MULTIPLICATION -> stack.push((int)stack.pop() * (int)stack.pop());
+                case IntermediateCodeNodeKind.DIVISION -> stack.push((int)stack.pop() / (int)stack.pop());
+                case IntermediateCodeNodeKind.LOGICAL_AND -> stack.push((boolean)stack.pop() && (boolean)stack.pop());
+                case IntermediateCodeNodeKind.LOGICAL_OR -> stack.push((boolean)stack.pop() || (boolean)stack.pop());
+                case IntermediateCodeNodeKind.EQUALS -> stack.push(stack.pop().equals(stack.pop()));
+                case IntermediateCodeNodeKind.NOT_EQUALS -> stack.push(!stack.pop().equals(stack.pop()));
+            }
         }
+    }
+
+    public Object getResult() {
+        return stack.pop();
     }
 
 }
